6015665747cb10b88b38417921659f4be0ba41a5
soar1688 <coo-fly@163.com>
2019-09-12 21:55:50 +0800

    Update Entrypoint.sh
    更新v 0.3.1

--- dockerfile/Entrypoint.sh
+++ dockerfile/Entrypoint.sh
@@ -1,16 +1,10 @@
 #!/bin/sh
 
-HFISH_DIR=/opt/hfish
+HFISH_DIR=/opt/HFish
 
 if [ ! -d $HFISH_DIR ];then
-  mv /tmp/hfish $HFISH_DIR
+  mv /tmp/HFish $HFISH_DIR
   sed -i "s/status = 0/status = 1/g" $HFISH_DIR/config.ini
-  sed -i "s/127.0.0.1/0.0.0.0/g" $HFISH_DIR/config.ini
-  sed -i "s/dart_report/deep_report/" $HFISH_DIR/web/deep/static/x.js
-fi
-
-if [ ! -z "$API_IP" ];then
-  sed -i "s/localhost:9001/$API_IP/" `find $HFISH_DIR/web -name x.js`
 fi
 
 if [ ! -z "$CLUSTER_IP" ];then
@@ -30,5 +24,15 @@ if [ ! -z "$PASSWORD" ];then
   sed -i "s/password = admin/password = $PASSWORD/" $HFISH_DIR/config.ini
 fi
 
-cd $HFISH_DIR && ./HFish run
+if [ ! -z "$MYSQL_USER" ] && [ ! -z "$MYSQL_PASSWORD" ] && [ ! -z "$MYSQL_IP" ] && [ ! -z "$MYSQL_PORT" ] && [ ! -z "$MYSQL_DATABASE" ];then
+  sed -i "s/db_type = sqlite/db_type = mysql/" $HFISH_DIR/config.ini
+  sed -i "s#^db_str = .*rwc#db_str = $MYSQL_USER:$MYSQL_PASSWORD@tcp\($MYSQL_IP:$MYSQL_PORT\)\/$MYSQL_DATABASE\?charset=utf8\&parseTime=true\&loc=Local#" $HFISH_DIR/config.ini
+  if [ ! -f $HFISH_DIR/db/sql/import_sql.log ];then
+    mysql -h $MYSQL_IP -P $MYSQL_PORT -u$MYSQL_USER -p$MYSQL_PASSWORD -D $MYSQL_DATABASE < $HFISH_DIR/db/sql/hfish_colony.sql &&
+    mysql -h $MYSQL_IP -P $MYSQL_PORT -u$MYSQL_USER -p$MYSQL_PASSWORD -D $MYSQL_DATABASE < $HFISH_DIR/db/sql/hfish_info.sql &&
+    mysql -h $MYSQL_IP -P $MYSQL_PORT -u$MYSQL_USER -p$MYSQL_PASSWORD -D $MYSQL_DATABASE < $HFISH_DIR/db/sql/hfish_setting.sql &&
+    echo "SQL import time: `date "+%Y-%m-%d %H:%M:%S"`" > $HFISH_DIR/db/sql/import_sql.log
+  fi
+fi
 
+cd $HFISH_DIR && ./HFish run
