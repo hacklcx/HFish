dbc3d8445f2dba3b19fdf35def844694b12dcfaf
SanJin <sanjinhub@gmail.com>
2020-05-12 19:55:22 +0800

    update

--- main.go
+++ main.go
@@ -17,7 +17,7 @@ func main() {
 		} else if args[1] == "init" || args[1] == "--init" {
 			setting.Init()
 		} else if args[1] == "version" || args[1] == "--version" {
-			fmt.Println("v0.6.2")
+			fmt.Println("v0.6.3")
 		} else if args[1] == "run" || args[1] == "--run" {
 			setting.Run()
 		} else {
