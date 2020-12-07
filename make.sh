cb3de8417fa5e733219e9994b04ff8e769eb733d
my417 <my417@hotmail.com>
2020-12-07 15:58:35 +0800

    [UPD]1.修复前端系统设置页面没有发送请求的问题，2.修复系统设置页面威胁情报测试接口没有显示返回结果的问题。3.更新docker镜像的打包脚本以及xgo的打包脚本

--- make.sh
+++ make.sh
@@ -1,8 +1,7 @@
-../xgo --targets=darwin/amd64 -ldflags='-w -s' .
-../xgo --targets=darwin/386 -ldflags='-w -s' .
-../xgo --targets=linux/arm64 -ldflags='-w -s' .
-../xgo --targets=linux/amd64 -ldflags='-w -s' . 
-../xgo --targets=linux/386 -ldflags='-w -s' . 
-../xgo --targets=linux/arm-7 -ldflags='-w -s' .
-../xgo --targets=windows/amd64 -ldflags='-w -s' .  
-../xgo --targets=windows/386 -ldflags='-w -s' .
+xgo --targets=darwin/amd64 -ldflags='-w -s' .
+xgo --targets=linux/arm64 -ldflags='-w -s' .
+xgo --targets=linux/amd64 -ldflags='-w -s' .
+xgo --targets=linux/386 -ldflags='-w -s' .
+xgo --targets=linux/arm-7 -ldflags='-w -s' .
+xgo --targets=windows/amd64 -ldflags='-w -s' .
+xgo --targets=windows/386 -ldflags='-w -s' .
