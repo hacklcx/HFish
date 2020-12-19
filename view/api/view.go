085ec1b37cb8b4a392ce8718f02086eba85441c1
Dustin Loring <dustinwloring1988@gmail.com>
2020-12-19 01:52:38 -0500

    Update view.go
    English Translation

--- view/api/view.go
+++ view/api/view.go
@@ -12,7 +12,7 @@ import (
 	"HFish/utils/log"
 )
 
-// 上报WEB蜜罐
+// Report WEB honeypot
 func ReportWeb(c *gin.Context) {
 	name := c.PostForm("name")
 	info := c.PostForm("info")
@@ -30,18 +30,18 @@ func ReportWeb(c *gin.Context) {
 		return
 	} else {
 
-		// 判断是否为 RPC 客户端
+		// Determine whether it is an RPC client
 		if is.Rpc() {
 			go client.ReportResult("WEB", name, ip, info, "0")
 		} else {
-			go report.ReportWeb(name, "本机", ip, info)
+			go report.ReportWeb(name, "Native", ip, info)
 		}
 
 		c.JSON(http.StatusOK, error.ErrSuccess)
 	}
 }
 
-// 上报暗网蜜罐
+// Report dark web honeypot
 func ReportDeepWeb(c *gin.Context) {
 	name := c.PostForm("name")
 	info := c.PostForm("info")
@@ -59,11 +59,11 @@ func ReportDeepWeb(c *gin.Context) {
 		return
 	} else {
 
-		// 判断是否为 RPC 客户端
+		// Determine whether it is an RPC client
 		if is.Rpc() {
 			go client.ReportResult("DEEP", name, ip, info, "0")
 		} else {
-			go report.ReportDeepWeb(name, "本机", ip, info)
+			go report.ReportDeepWeb(name, "Native", ip, info)
 		}
 
 		c.JSON(http.StatusOK, error.ErrSuccess)
@@ -77,13 +77,13 @@ type PlugInfo struct {
 	Info   map[string]interface{} `json:"info"`
 }
 
-// 蜜罐插件API
+// Honeypot plugin API
 func ReportPlugWeb(c *gin.Context) {
 	var info PlugInfo
 	err := c.BindJSON(&info)
 
 	if err != nil {
-		log.Pr("HFish", "127.0.0.1", "插件上报信息错误", err)
+		log.Pr("HFish", "127.0.0.1", "Plug-in report error", err)
 
 		c.JSON(http.StatusOK, gin.H{
 			"code": error.ErrFailPlug["code"],
@@ -114,18 +114,18 @@ func ReportPlugWeb(c *gin.Context) {
 		return
 	} else {
 
-		// 判断是否为 RPC 客户端
+		// Determine whether it is an RPC client
 		if is.Rpc() {
 			go client.ReportResult("PLUG", info.Name, info.Ip, data, "0")
 		} else {
-			go report.ReportPlugWeb(info.Name, "本机", info.Ip, data)
+			go report.ReportPlugWeb(info.Name, "Native", info.Ip, data)
 		}
 
 		c.JSON(http.StatusOK, error.ErrSuccess)
 	}
 }
 
-// 获取黑名单 黑客IP 列表
+// Get blacklist hacker IP list
 func GetIpList(c *gin.Context) {
 	key, _ := c.GetQuery("key")
 
@@ -138,14 +138,14 @@ func GetIpList(c *gin.Context) {
 		result, err := dbUtil.DB().Table("hfish_info").Fields("ip").GroupBy("ip").Get()
 
 		if err != nil {
-			log.Pr("API", "127.0.0.1", "查询黑名单IP列表失败", err)
+			log.Pr("API", "127.0.0.1", "Failed to query the blacklist IP list", err)
 		}
 
 		c.JSON(http.StatusOK, error.ErrSuccessWithData(result))
 	}
 }
 
-// 获取钓鱼列表 API
+// Get phishing list API
 func GetFishInfo(c *gin.Context) {
 	key, _ := c.GetQuery("key")
 
@@ -158,14 +158,14 @@ func GetFishInfo(c *gin.Context) {
 		result, err := dbUtil.DB().Table("hfish_info").OrderBy("id desc").Get()
 
 		if err != nil {
-			log.Pr("API", "127.0.0.1", "获取钓鱼列表失败", err)
+			log.Pr("API", "127.0.0.1", "Failed to get phishing list", err)
 		}
 
 		c.JSON(http.StatusOK, error.ErrSuccessWithData(result))
 	}
 }
 
-// 获取账号密码列表 API
+// Get account password list API
 func GetAccountPasswdInfo(c *gin.Context) {
 	key, _ := c.GetQuery("key")
 
@@ -178,7 +178,7 @@ func GetAccountPasswdInfo(c *gin.Context) {
 		result, err := dbUtil.DB().Table("hfish_passwd").OrderBy("id desc").Get()
 
 		if err != nil {
-			log.Pr("API", "127.0.0.1", "获取账号密码列表失败", err)
+			log.Pr("API", "127.0.0.1", "Failed to get account password list", err)
 		}
 
 		c.JSON(http.StatusOK, error.ErrSuccessWithData(result))
