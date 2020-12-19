60cce24ab9c8c11bc0b9948bfc3441837e01acb2
Dustin Loring <dustinwloring1988@gmail.com>
2020-12-19 00:16:58 -0500

    Update error.go
    English Translation

--- error/error.go
+++ error/error.go
@@ -6,19 +6,19 @@ import (
 )
 
 var (
-	ErrSuccess = gin.H{"code": 200,	"msg": "成功"}
-	ErrFailApiKey = gin.H{"code": 1001, "msg": "秘钥不正确"}
-	ErrFailLogin = gin.H{"code": 1002, "msg": "账号密码不正确"}
-	ErrFailMail = gin.H{"code": 1003, "msg": "邮箱未启用"}
-	ErrFailConfig = gin.H{"code": 1004, "msg": "请配置后再启用"}
-	ErrFailPlug = gin.H{"code": 1005, "msg": "上报信息错误"}
-	ErrInputData = gin.H{"code": 1006, "msg": "请求数据非法"}
-	ErrUpdateData = gin.H{"code": 1007,	"msg": "数据更新失败"}
-	ErrDeleteData = gin.H{"code": 1008,	"msg": "数据清除失败"}
-	ErrTestSyslog = gin.H{"code": 1009, "msg": "测试Syslog发送失败"}
-	ErrTestEmail = gin.H{"code": 1010, "msg": "测试邮件发送失败"}
-	ErrTestIntelligence = gin.H{"code": 1011, "msg": "测试获取威胁情报失败"}
-	ErrExportData = gin.H{"code": 1012,	"msg": "数据导出失败"}
+	ErrSuccess = gin.H{"code": 200,	"msg": "success"}
+	ErrFailApiKey = gin.H{"code": 1001, "msg": "Incorrect key"}
+	ErrFailLogin = gin.H{"code": 1002, "msg": "Incorrect account password"}
+	ErrFailMail = gin.H{"code": 1003, "msg": "Mailbox is not enabled"}
+	ErrFailConfig = gin.H{"code": 1004, "msg": "Please configure and enable"}
+	ErrFailPlug = gin.H{"code": 1005, "msg": "Reported information error"}
+	ErrInputData = gin.H{"code": 1006, "msg": "The requested data is illegal"}
+	ErrUpdateData = gin.H{"code": 1007, "msg": "Data update failed"}
+	ErrDeleteData = gin.H{"code": 1008, "msg": "Data deletion failed"}
+	ErrTestSyslog = gin.H{"code": 1009, "msg": "Failed to send test Syslog"}
+	ErrTestEmail = gin.H{"code": 1010, "msg": "Failed to send test email"}
+	ErrTestIntelligence = gin.H{"code": 1011, "msg": "Test failed to obtain threat intelligence"}
+	ErrExportData = gin.H{"code": 1012, "msg": "Data export failed"}
 )
 
 func ErrSuccessWithData(data interface{}) gin.H {
