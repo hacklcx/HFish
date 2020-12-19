fd5ce997e17bebe7dcbedfeae2f765c0a9faec9e
Dustin Loring <dustinwloring1988@gmail.com>
2020-12-19 00:43:24 -0500

    Update page.go
    English  Translation

--- utils/page/page.go
+++ utils/page/page.go
@@ -1,16 +1,16 @@
 package page
 
-// 分页从1开始
+// Paging starts from 1
 func Start(currentPage int, pageSize int) int {
 	return (currentPage - 1) * pageSize
 }
 
-// 分页结束
+// End of page
 func End(currentPage int64, pageSize int64) int64 {
 	return currentPage * pageSize
 }
 
-// 分页总页数
+// Total number of pages
 func TotalPage(count int, pageSize int) int {
 	result := count / pageSize
 	yu := count % pageSize
