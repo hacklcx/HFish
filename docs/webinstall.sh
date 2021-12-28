c51132f5da9e836161a14e69316c587dfa6a0eb2
my417 <my417@hotmail.com>
2021-12-28 20:29:01 +0800

    webinstall.sh改成英文的了，希望大家都看得懂

--- docs/webinstall.sh
+++ docs/webinstall.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#初始化
+#init
 initVar() {
 	installType='yum -y install'
 	removeType='yum -y remove'
@@ -11,30 +11,25 @@ initVar() {
 initVar
 export LANG=en_US.UTF-8
 
-#字体颜色
+#the color of font
 echoContent() {
 	case $1 in
-	# 红色
 	"red")
 		# shellcheck disable=SC2154
 		${echoType} "\033[31m${printN}$2 \033[0m"
 		;;
-		# 天蓝色
 	"skyBlue")
 		${echoType} "\033[1;36m${printN}$2 \033[0m"
 		;;
-		# 绿色
 	"green")
 		${echoType} "\033[32m${printN}$2 \033[0m"
 		;;
-		# 白色
 	"white")
 		${echoType} "\033[37m${printN}$2 \033[0m"
 		;;
 	"magenta")
 		${echoType} "\033[31m${printN}$2 \033[0m"
 		;;
-		# 黄色
 	"yellow")
 		${echoType} "\033[33m${printN}$2 \033[0m"
 		;;
@@ -49,21 +44,19 @@ menu() {
     echoContent red "| |_| | | |_    | | / __| | '_ \ "
     echoContent red "|  _  | |  _|   | | \__ \ | | | |"
     echoContent red "|_| |_| |_|     |_| |___/ |_| |_|"
-    echoContent green "当前版本：v${version}"
+    echoContent green "v${version}"
     echoContent green "https://hfish.io \n"
     echoContent red "----------"
-    echoContent yellow "按 1 安装并运行HFish"
-    echoContent yellow "按 2 退出脚本"
-	# echoContent skyBlue "\n-------------------------配置管理-----------------------------\n"
-	# echoContent yellow "5.防火墙放通管理端口（coming soon）"
-	# echoContent yellow "6.将HFish添加为系统服务（coming soon）"
-    # echoContent skyBlue "\n-------------------------运维管理-----------------------------\n"
-	# echoContent yellow "8.将错误日志反馈给开发者（coming soon）"
-	# echoContent yellow "9.卸载HFish（coming soon）"
+    echoContent yellow "Press 1 : Install and run HFish"
+	# echoContent yellow "Press 2 : Add mgmt ports to the firewall（coming soon）"
+	# echoContent yellow "Press 3 : Install as service（coming soon）"
+	# echoContent yellow "Press 4 : Post error logfile to HFish team（coming soon）"
+	# echoContent yellow "Press 9 : Uninstall HFish（coming soon）"
+    echoContent yellow "Press 0 : Exit"
 	echoContent red "----------"
 
 	while [ 1 ]; do
-		read -r -p "请选择:" selectMenuType
+		read -r -p "Input: " selectMenuType
 
 		case ${selectMenuType} in
 		1):
@@ -87,7 +80,7 @@ serverInstall() {
 	elif [ $(uname -m) = 'aarch64' ] && [ $(getconf LONG_BIT) = '64' ]; then
     curl -k http://hfish.cn-bj.ufileos.com/hfish-${version}-linux-arm64.tgz hfish-${version}-linux-arm64.tgz
 	else
-    echoContent red "未检测到系统版本，请参阅 https://hfish.io 官网文档手动安装！\n" && exit 1
+    echoContent red "No OS version is detected. Please refer to https://hfish.io for manual installation\n" && exit 1
 	fi
 
 	mkdir -p hfish
