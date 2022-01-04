7d6144e698dcce412b86652e812d606336132d69
my417 <my417@hotmail.com>
2022-01-04 22:25:38 +0800

    修复webinstall.sh的一个小bug

--- docs/webinstall.sh
+++ docs/webinstall.sh
@@ -39,10 +39,10 @@ echoContent() {
 
 #start
 menu() {
-    echoContent red " _   _   _____   _         _     " 
-    echoContent red "| | | | |  ___| (_)  ___  | |__  "
-    echoContent red "| |_| | | |_    | | / __| | '_ \ "
-    echoContent red "|  _  | |  _|   | | \__ \ | | | |"
+	echoContent red " _   _   _____   _         _     " 
+	echoContent red "| | | | |  ___| (_)  ___  | |__  "
+	echoContent red "| |_| | | |_    | | / __| | '_ \ "
+	echoContent red "|  _  | |  _|   | | \__ \ | | | |"
 	echoContent red "|_| |_| |_|     |_| |___/ |_| |_|  v${version}"
 	echoContent green "https://hfish.io\n\n"
 	echoContent white "Press 1 : Install and run HFish"
@@ -50,7 +50,7 @@ menu() {
 	# echoContent yellow "Press 3 : Install as service（coming soon）"
 	# echoContent yellow "Press 4 : Post error logfile to HFish team（coming soon）"
 	# echoContent yellow "Press 9 : Uninstall HFish（coming soon）"
-    echoContent white "Press 0 : Exit"
+	echoContent white "Press 0 : Exit"
 	echoContent white "----------"
 
 	while [ 1 ]; do
@@ -72,17 +72,17 @@ menu() {
 }
 
 serverInstall() {
-  cd /opt
+	cd /opt
 	if [ $(uname -s) = 'Linux' ] && [ $(uname -m) = 'x86_64' ] && [ $(getconf LONG_BIT) = '64' ]; then
-    curl -k http://hfish.cn-bj.ufileos.com/hfish-${version}-linux-amd64.tgz -o hfish-${version}-linux-amd64.tgz
+		curl -k http://hfish.cn-bj.ufileos.com/hfish-${version}-linux-amd64.tgz -o hfish-${version}-linux-amd64.tgz
 	elif [ $(uname -m) = 'aarch64' ] && [ $(getconf LONG_BIT) = '64' ]; then
-    curl -k http://hfish.cn-bj.ufileos.com/hfish-${version}-linux-arm64.tgz hfish-${version}-linux-arm64.tgz
+		curl -k http://hfish.cn-bj.ufileos.com/hfish-${version}-linux-arm64.tgz hfish-${version}-linux-arm64.tgz
 	else
-    echoContent red "No OS version is detected. Please refer to https://hfish.io for manual installation\n" && exit 1
+		echoContent red "No OS version is detected. Please refer to https://hfish.io for manual installation\n" && exit 1
 	fi
 
 	mkdir -p hfish
-	tar -zxvf /opt/hfish*.tgz -C hfish
+	tar -zxvf /opt/hfish-${version}*.tgz -C hfish
 	cd hfish
 	sudo ./install.sh
 }
